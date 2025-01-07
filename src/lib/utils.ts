9bc1d0cdf2f8dbb3e833af4813ea646440877458
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2025-01-07 21:08:11 +0000

    UPDATE: utils.ts to EUR

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatCurrency = (amount: number) => {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('en-IE', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'EUR'
   }).format(amount);
 };
